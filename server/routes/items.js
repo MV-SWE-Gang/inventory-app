5b87c36275016af1a397924e8d5160c65101200c
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-03-07 14:58:55 +0000

    Express routes complete

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { where } = require("sequelize");
 const router = express.Router();
 const { Item } = require("../models");
+const {check, validationResult} = require("express-validator");
 
 // GET /items
 router.get("/", async (req, res, next) => {
@@ -15,6 +17,33 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const items = await Item.findByPk(req.params.id);
+    res.send(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const items = await Item.create(req.body);
+    res.send(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Item.destroy({where: {id: req.params.id}});
+    res.send("deleted ;)");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const items = await Item.update(req.body, {where: {id: req.params.id}});
     res.send(items);
   } catch (error) {
     next(error);
