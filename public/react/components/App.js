42aea6809a92f04e8bbd63d0e14d8f62681753ef
Daniel-multiverse <daniel.oyewole@multiverse.io>
2023-03-08 10:53:31 +0000

    commented out sauces function

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
-import { ItemsList } from './ItemsList';
+// import { SaucesList } from './SaucesList';
+import { ItemList } from './ItemsList';
 
 
 // import and prepend the api url to any fetch calls
@@ -11,16 +11,16 @@ export const App = () => {
 	const [sauces, setSauces] = useState([]);
 	const [items, setItems] = useState([]);
 
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
+	// async function fetchSauces(){
+	// 	try {
+	// 		const response = await fetch(`${apiURL}/sauces`);
+	// 		const saucesData = await response.json();
 			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+	// 		setSauces(saucesData);
+	// 	} catch (err) {
+	// 		console.log("Oh no an error! ", err)
+	// 	}
+	// }
 
 	async function fetchItems(){
 		try {
@@ -59,7 +59,7 @@ export const App = () => {
 	}
 
 	useEffect(() => {
-		fetchSauces();
+		// fetchSauces();
 		fetchItems()
 	}, []);
 
@@ -67,8 +67,8 @@ export const App = () => {
 		<main>	
       <h1>Sauce Store</h1>
 			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
-			<ItemsList items={items} />
+			{/* <SaucesList sauces={sauces} /> */}
+			<ItemList items={items} />
 		</main>
 	)
 }
